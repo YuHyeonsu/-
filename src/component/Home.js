756b563017a3903bdb6d60b092557fb9314e6121
hwiyoon20010309 <boksil0101@gmail.com>
2024-07-19 17:19:36 +0900

    최신순, 인기순 정렬 코드 추가

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import mockData from "../mock.json";
 
 function Home() {
   const navigate = useNavigate();
+  const [sortBy, setSortBy] = useState("");
 
   const handleNavigate = (path) => {
     navigate(path);
   };
 
-  const topics = mockData.map((topic) => (
+  const handleSortByLatest = () => {
+    setSortBy("latest");
+  };
+
+  const handleSortByPopularity = () => {
+    setSortBy("popularity");
+  };
+
+  const sortByLatest = (data) => {
+    return [...data].sort(
+      (a, b) => new Date(b.uploadDate) - new Date(a.uploadDate)
+    );
+  };
+
+  const sortByPopularity = (data) => {
+    return [...data].sort((a, b) => b.opinionsNumber - a.opinionsNumber);
+  };
+
+  const sortedTopics =
+    sortBy === "latest"
+      ? sortByLatest(mockData)
+      : sortBy === "popularity"
+      ? sortByPopularity(mockData)
+      : mockData;
+
+  const topics = sortedTopics.map((topic) => (
     <div
       key={topic.id}
       className="topic-item"
@@ -38,11 +64,17 @@ function Home() {
       </header>
       <main className="main-content">
         <section className="topic-section">
-          <h2>토론중인 주제 </h2>
+          <h2>토론중인 주제</h2>
           <h4>
             정렬순서
-            <span className="view-all"> · 인기순</span>{" "}
-            <span className="view-all"> · 최신순</span>
+            <span className="view-all" onClick={handleSortByPopularity}>
+              {" "}
+              · 인기순
+            </span>{" "}
+            <span className="view-all" onClick={handleSortByLatest}>
+              {" "}
+              · 최신순
+            </span>
           </h4>
           <div className="home-category">
             <h4>
